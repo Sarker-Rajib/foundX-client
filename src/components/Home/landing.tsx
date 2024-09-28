de25d0b5338bf1f67d108e3e8acd6d782ee28133
Rajib Sarker <redhat0593@gmail.com>
2024-09-29 00:38:38 +0600

    data added

--- src/components/Home/landing.tsx
+++ src/components/Home/landing.tsx
@@ -1,7 +1,8 @@
 export default function HomeLanding() {
   return (
     <div className="min-h-[400px] p-2 bg-teal-700">
-      <h2>Landing</h2>
+      <h2>Landing page</h2>
+      <h2>Landing component</h2>
     </div>
   );
 };
--- src/components/Home/recentItems.tsx
+++ src/components/Home/recentItems.tsx
@@ -1,19 +1,15 @@
 import { getRecentPosts } from "@/src/services/RecentPosts";
-import Image from "next/image";
+import MyCard from "../card";
+import { IPost } from "@/src/types";
 
 export default async function RecentItems() {
-  const { data: posts } = await getRecentPosts();
+  const { data }: { data: IPost[] } = await getRecentPosts();
 
   return (
     <div>
       <h2>Recent Items</h2>
       {
-        posts.map((item, i) => <div key={i}>
-          <p>{item.title}</p>
-          {
-            item.images.map((url, i) => <Image width={400} height={400} key={i} src={url} alt="image" />)
-          }
-        </div>)
+        data.map((item, i: number) => <MyCard key={i} post={item} />)
       }
     </div>
   );
