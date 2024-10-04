de4a7a03b051c3e6cc17cdf888f4d6a671ea8d1d
Rajib Sarker <redhat0593@gmail.com>
2024-10-04 19:51:27 +0600

    u

--- src/middleware.ts
+++ src/middleware.ts
@@ -13,6 +13,8 @@ const RoleBasedRoutes: Record<string, RegExp[]> = {
   ADMIN: [/^\/admin/],
 };
 
+type TRole = keyof typeof RoleBasedRoutes
+
 export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
 
@@ -33,8 +35,8 @@ export function middleware(request: NextRequest) {
   }
 
   // Check if the user's role has any specific routes
-  if (user.role && RoleBasedRoutes[user.role]) {
-    const routes = RoleBasedRoutes[user.role];
+  if (user?.role && RoleBasedRoutes[user?.role]) {
+    const routes = RoleBasedRoutes[user?.role];
 
     // Check if the current pathname matches any of the routes
     if (routes.some((route) => pathname.match(route))) {
@@ -49,3 +51,54 @@ export function middleware(request: NextRequest) {
 export const config = {
   matcher: ['/admin', "/login", "/register", '/profile'],
 };
+
+
+// import { NextResponse } from "next/server";
+// import { NextRequest } from "next/server";
+
+// const AuthRoutes = ["/login", "/register"];
+
+// type Role = keyof typeof roleBasedRoutes;
+
+// const roleBasedRoutes = {
+//   USER: [/^\/profile/],
+//   ADMIN: [/^\/admin/],
+// };
+
+// // This function can be marked `async` if using `await` inside
+// export function middleware(request: NextRequest) {
+//   const { pathname } = request.nextUrl;
+
+//   console.log(pathname);
+
+//   //   const user = {
+//   //     name: "Mir",
+//   //     token: "adsf asda",
+//   //     role: "ADMIN",
+//   //   };
+
+//   const user = undefined;
+
+//   if (!user) {
+//     if (AuthRoutes.includes(pathname)) {
+//       return NextResponse.next();
+//     } else {
+//       return NextResponse.redirect(new URL("/login", request.url));
+//     }
+//   }
+
+//   if (user?.role && roleBasedRoutes[user?.role as Role]) {
+//     const routes = roleBasedRoutes[user?.role as Role];
+
+//     if (routes.some((route) => pathname.match(route))) {
+//       return NextResponse.next();
+//     }
+//   }
+
+//   return NextResponse.redirect(new URL("/", request.url));
+// }
+
+// // See "Matching Paths" below to learn more
+// export const config = {
+//   matcher: ["/profile", "/admin", "/login", "/register"],
+// };
