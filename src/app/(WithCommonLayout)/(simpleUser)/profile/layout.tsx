8b186d3308dae2674f926f72161d318a3f3d730d
Rajib Sarker <redhat0593@gmail.com>
2024-10-06 22:56:15 +0600

    comment

--- src/app/(WithCommonLayout)/(simpleUser)/profile/layout.tsx
+++ src/app/(WithCommonLayout)/(simpleUser)/profile/layout.tsx
@@ -5,6 +5,7 @@ const UserLayout = ({ children }: { children: ReactNode }) => {
   return (
     <div className="max-w-[1280px] mx-auto p-2 grid grid-cols-4">
       <div>
+        {/* sidebar */}
         <Sidebar />
       </div>
       <div className="col-span-3">
